b6aa090e31452ffc5c3eed2f95809542737746f9
maryna <merry.kosiv@gmail.com>
2021-01-14 18:21:45 +0200

    components folders

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import SideMenu from "../components/SideMenu";
+
+function App() {
+    return (
+        <div>
+            <SideMenu/>
+            <div>here we go</div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/SideMenu.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const SideMenu = () => {
+    return (
+        <div>side menu</div>
+    )
+};
+
+export default SideMenu;
