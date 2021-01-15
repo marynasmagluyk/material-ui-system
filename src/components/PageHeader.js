ca7d370044fa726f8fbd7b9e1b14796cd9d6cf17
maryna <merry.kosiv@gmail.com>
2021-01-15 16:59:45 +0200

    styled pageHeader component

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import {Paper, Card, Typography, makeStyles} from '@material-ui/core';
 
+const useStyles = makeStyles(theme => ({
+    root: {
+        backgroundColor: '#a7c7f0',
+    },
+    pageHeader: {
+        padding: theme.spacing(4),
+        display: 'flex',
+        marginBottom: theme.spacing(2)
+    },
+    pageIcon: {
+        display: 'inline-block',
+        padding: theme.spacing(2),
+        color: '#3c44b1',
+    },
+    pageTitle: {
+        paddingLeft: theme.spacing(4),
+        '& .MuiTypography-subtitle2': {
+            opacity: '0.6',
+        }
+    }
+}));
+
 
 const PageHeader = (props) => {
 
+    const classes = useStyles();
     const {title, subtitle, icon} = props;
 
     return (
         <Paper elevation={0}
-               square>
-            <div>
-                <Card> {icon}</Card>
-                <div>
+               square
+               className={classes.root}>
+            <div className={classes.pageHeader}>
+                <Card className={classes.pageIcon}> {icon}</Card>
+                <div className={classes.pageTitle}>
                     <Typography
                         variant='h6'
                         component='div'
@@ -19,7 +43,7 @@ const PageHeader = (props) => {
                         {title}
                     </Typography>
                     <Typography
-                        variant='subtitle'
+                        variant='subtitle2'
                         component='div'
                     >
                         {subtitle}
