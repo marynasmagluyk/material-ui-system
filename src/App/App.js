c04953ad68aaee9eb301da3add4c670f984c9560
maryna <merry.kosiv@gmail.com>
2021-01-15 12:26:01 +0200

    main styles

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from 'react';
 import './App.css';
 import SideMenu from "../components/SideMenu";
+import {makeStyles} from '@material-ui/core';
+
+
+const useStyles = makeStyles({
+    appStyles: {
+        paddingLeft: '320px',
+        width: '100%'
+    }
+});
+
 
 function App() {
+
+    const classes = useStyles();
+
     return (
         <div>
             <SideMenu/>
-            <div>here we go</div>
+            <div className={classes.appStyles}>here we go</div>
         </div>
     );
 }
