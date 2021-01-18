5c7b899a6e2be7becf1a9f11be99f4f05965deff
maryna <merry.kosiv@gmail.com>
2021-01-18 15:38:46 +0200

    made input reusable component

--- src/pages/EmployeeForm.js
+++ src/pages/EmployeeForm.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {Grid, TextField, FormControl, RadioGroup, FormControlLabel, Radio, FormLabel} from '@material-ui/core';
 import {useForm, Form} from "../components/useForm";
+import Input from "../components/controls/Input";
 
 
 const initialFormValues = {
@@ -20,46 +21,41 @@ const EmployeeForm = () => {
     const {employees, setEmployees, handleInputChange} = useForm(initialFormValues);
 
 
-    // useEffect(() => {
-    //
-    // }, [employees]);
-
     return (
         <Form>
             <Grid container>
                 <Grid item
                       xs={6}>
-                    <TextField
-                        variant='outlined'
-                        label='Full Name'
-                        name='fullName'
-                        value={employees.fullName}
-                        onChange={handleInputChange}>
-                    </TextField>
-                    <TextField
+                    <Input
+                    name='fullName'
+                    label='Full Name'
+                    value={employees.fullName}
+                    onChange={handleInputChange}/>
+                    <Input
                         variant='outlined'
                         label='Email'
                         value={employees.email}>
-                    </TextField>
+                    </Input>
+                </Grid>
                     <Grid item
                           xs={6}>
                         <FormControl>
                             <FormLabel>Gender</FormLabel>
                             <RadioGroup row
                                         name='gender'
-                                        value={employees.fullName}
-                                        onChange={handleInputChange}></RadioGroup>
-                            <FormControlLabel value='female'
-                                              control={<Radio/>}
-                                              label='Female'></FormControlLabel>
+                                        value={employees.gender}
+                                        onChange={handleInputChange}>
                             <FormControlLabel value='male'
                                               control={<Radio/>}
-                                              label='Male'></FormControlLabel>
+                                              label='Male'/>
+                            <FormControlLabel value='female'
+                                              control={<Radio/>}
+                                              label='Female'/>
                             <FormControlLabel value='other'
                                               control={<Radio/>}
-                                              label='Other'></FormControlLabel>
+                                              label='Other'/>
+                            </RadioGroup>
                         </FormControl>
-                    </Grid>
                 </Grid>
             </Grid>
         </Form>
