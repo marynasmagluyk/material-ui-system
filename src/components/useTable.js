9061d865dd81d4e85e1bd53d0f9dd8662fd9bce6
maryna <merry.kosiv@gmail.com>
2021-01-20 17:33:56 +0200

    functional: recordsAfterPagingAndSorting in order

--- src/components/useTable.js
+++ src/components/useTable.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import {Table, TableHead, TableRow, TableCell, makeStyles, TablePagination} from '@material-ui/core';
+import {Table, TableHead, TableRow, TableCell, makeStyles, TablePagination, TableSortLabel} from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
     table: {
@@ -25,6 +25,9 @@ const useTable = (records, headCells) => {
     const pages = [5, 10, 20];
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(pages[page]);
+    const [order, setOrder] = useState();
+    const [orderBy, setOrderBy] = useState();
+
 
     const TableContainer = (props) => (
         <Table className={classes.table}>
@@ -33,14 +36,30 @@ const useTable = (records, headCells) => {
     );
 
     const TblHead = props => {
+
+        const handleSortRequest = (cellId) => {
+            const isAscending = orderBy === cellId && order === 'asc';
+            setOrder(isAscending ? 'desc' : 'asc');
+            setOrderBy(cellId);
+        };
+
         return (
             <TableHead>
                 <TableRow>
                     {
-                        headCells.map((headCell)=> {
+                        headCells.map((headCell) => {
                             const {id, label} = headCell;
                             return (
-                                <TableCell key={id}>{label}</TableCell>
+                                <TableCell key={id}>
+                                    <TableSortLabel
+                                        active={orderBy === id}
+                                        direction={orderBy === headCell.id ? order : 'asc'}
+                                        onClick={() => {
+                                            handleSortRequest(id)
+                                        }}>
+                                        {label}
+                                    </TableSortLabel>
+                                </TableCell>
                             )
                         })
                     }
@@ -54,23 +73,48 @@ const useTable = (records, headCells) => {
     };
 
     const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(parseInt(event.target.value,10));
+        setRowsPerPage(parseInt(event.target.value, 10));
         setPage(0);
     };
 
     const TblPagination = () => (
         <TablePagination
-        component='div'
-        page={page}
-        rowsPerPageOptions={pages}
-        rowsPerPage={rowsPerPage}
-        count={records.length}
-        onChangePage={handleChangePage}
-        onChangeRowsPerPage={handleChangeRowsPerPage}/>
+            component='div'
+            page={page}
+            rowsPerPageOptions={pages}
+            rowsPerPage={rowsPerPage}
+            count={records.length}
+            onChangePage={handleChangePage}
+            onChangeRowsPerPage={handleChangeRowsPerPage}/>
     );
 
+    const stableSort = (array, comparator) => {
+        const stabilizeThis = array.map((el, index) => [el, index]);
+        stabilizeThis.sort((a, b) => {
+            const order = comparator(a[0], b[0]);
+            if (order !== 0) return order;
+            return a[1] - b[1];
+        });
+        return stabilizeThis.map((el) => el[0])
+    };
+
+    const getComparator = (order, orderBy) => {
+        return order === 'desc'
+            ? (a, b) => descendingComparator(a, b, orderBy)
+            : (a, b) => -descendingComparator(a, b, orderBy)
+    };
+    const descendingComparator = (a, b, orderBy) => {
+        if (b[orderBy] < a[orderBy]) {
+            return -1;
+        }
+        if (b[orderBy] < a[orderBy]) {
+            return 1;
+        }
+        return 0;
+    };
+
     const recordsAfterPagingAndSorting = () => {
-        return records.slice(page*rowsPerPage, (page+1)*rowsPerPage)
+        return stableSort(records, getComparator(order, orderBy)).slice(page * rowsPerPage, (page + 1) * rowsPerPage)
     };
 
     return {
